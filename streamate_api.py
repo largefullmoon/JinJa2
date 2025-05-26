0c182ced3a101abdb8ed82f4027db77265fbab69
largefullmoon <largefullmoon@gmail.com>
2025-05-27 02:01:49 +0300

    update for fixing bugs

--- streamate_api.py
+++ streamate_api.py
@@ -11,7 +11,8 @@ def get_performers(
     ethnicity=None,
     fetishes=None,
     gender=None,
-    keyword=None
+    keyword=None,
+    fetish=None
 ):
     # Build XML request
     root = etree.Element("SMLQuery")
